77be008943421f1fc64b99479a431dc81a34a4e6
Vadzim <vadi.vad.dvp@gmail.com>
2020-02-28 20:03:11 +0300

    v.1

---
+++ Ing_lay_1.py
@@ -0,0 +1,104 @@
+import urllib.request
+import shutil
+import os
+import time
+
+NAME = "Project"
+URL = "http://files.grouplens.org/datasets/movielens/ml-latest.zip"
+PATH = os.getcwd() + "/" + NAME
+FILENAME = URL[URL.rfind("/") + 1:]
+FILEPATH = PATH + "/" + FILENAME
+FOLDER = URL[URL.rfind("/"):-4]
+
+
+def make_dir():
+    """
+
+    :return: Make directory named NAME if directory exists named with number
+    at the end
+    """
+    start_time = time.time()
+    counter = 1
+    global NAME, PATH, FILEPATH
+    while True:
+        if NAME not in os.listdir(os.getcwd()):
+            os.mkdir(NAME)
+            print("Make directory {}".format(NAME))
+            print("Take {} seconds".format(time.time() - start_time), "\n")
+            break
+        else:
+            NAME = "Project_{}".format(counter)
+            PATH = os.getcwd() + "/" + NAME
+            FILEPATH = PATH + "/" + FILENAME
+            counter += 1
+            continue
+
+
+def download(filepath, url=URL):
+    """
+
+    :param filepath: str, file path relative
+    :param url: str, take a file url
+    :return: file, download file from url to filepath
+    """
+    start_time = time.time()
+    print("Start download {}".format(FILENAME))
+    urllib.request.urlretrieve(url, filepath)
+    print("Download {} complete".format(FILENAME))
+    print("Take {} seconds".format(time.time() - start_time), "\n")
+
+
+def unpack(filepath, path):
+    """
+
+    :param filepath: str, file path relative
+    :param path: str, path relative
+    :return: directory, unpack a file from file path to path
+    """
+    start_time = time.time()
+    shutil.unpack_archive(filepath, path)
+    print("Unpack {} complete".format(FILENAME))
+    print("Take {} seconds".format(time.time() - start_time), "\n")
+
+
+def create(path, name):
+    """
+
+    :param path: str, path relative :param name: str, name directory
+    :return: directories, create directories (ex:
+    /Project/tmp/datalake/movies/movies.csv)for all csv files from unpack
+    the directory and move csv files to them
+    """
+    start_time = time.time()
+    os.chdir(path)
+    print("Change directory to {}".format(name))
+    for i in os.listdir(os.getcwd() + FOLDER):
+        if i[-3:] == "csv":
+            os.makedirs("tmp/datalake/{}".format(i[:-4]))
+            shutil.move(os.getcwd() + FOLDER + "/" + i,
+                        path + "/tmp/datalake/{}".format(i[:-4]))
+            print("{} directory create".format(i[:-4]))
+    print("Take {} seconds".format(time.time() - start_time), "\n")
+
+
+def remove(filepath):
+    """
+
+    :param filepath: str, file path relative
+    :return: nothing, remove not used files/directories
+    """
+    start_time = time.time()
+    os.remove(filepath)
+    print("Remove {} complete".format(FILENAME))
+    shutil.rmtree(os.getcwd() + FOLDER)
+    print("Remove {} complete".format(FOLDER))
+    print("Take {} seconds".format(time.time() - start_time), "\n")
+
+
+start_time = time.time()
+make_dir()
+download(FILEPATH)
+unpack(FILEPATH, PATH)
+create(PATH, NAME)
+remove(FILEPATH)
+print("Script take {} seconds".format(time.time() - start_time))
