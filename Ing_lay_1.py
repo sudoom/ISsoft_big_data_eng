16448f9dd608aa01b4d0e8b0f3799e00d1ef93d8
Vadzim <vadi.vad.dvp@gmail.com>
2020-03-08 18:52:11 +0300

    v.1.0.1

--- Ing_lay_1.py
+++ Ing_lay_1.py
@@ -11,9 +11,10 @@ FILEPATH = PATH + "/" + FILENAME
 FOLDER = URL[URL.rfind("/"):-4]
 
 
-def make_dir():
+def make_dir(debug=0):
     """
 
+    :param debug: debug flag
     :return: Make directory named NAME if directory exists named with number
     at the end
     """
@@ -23,8 +24,9 @@ def make_dir():
     while True:
         if NAME not in os.listdir(os.getcwd()):
             os.mkdir(NAME)
-            print("Make directory {}".format(NAME))
-            print("Take {} seconds".format(time.time() - start_time), "\n")
+            if debug == 1:
+                print("Make directory {}".format(NAME))
+                print("Take {} seconds".format(time.time() - start_time), "\n")
             break
         else:
             NAME = "Project_{}".format(counter)
@@ -34,71 +36,90 @@ def make_dir():
             continue
 
 
-def download(filepath, url=URL):
+def download(filepath, url=URL, debug=0):
     """
 
+    :param debug: debug flag
     :param filepath: str, file path relative
     :param url: str, take a file url
     :return: file, download file from url to filepath
     """
     start_time = time.time()
-    print("Start download {}".format(FILENAME))
+    # print("Start download {}".format(FILENAME))
     urllib.request.urlretrieve(url, filepath)
-    print("Download {} complete".format(FILENAME))
-    print("Take {} seconds".format(time.time() - start_time), "\n")
+    if debug == 1:
+        print("Download {} complete".format(FILENAME))
+        print("Take {} seconds".format(time.time() - start_time), "\n")
 
 
-def unpack(filepath, path):
+def unpack(filepath, path, debug=0):
     """
 
+    :param debug: debug flag
     :param filepath: str, file path relative
     :param path: str, path relative
     :return: directory, unpack a file from file path to path
     """
     start_time = time.time()
     shutil.unpack_archive(filepath, path)
-    print("Unpack {} complete".format(FILENAME))
-    print("Take {} seconds".format(time.time() - start_time), "\n")
+    if debug == 1:
+        print("Unpack {} complete".format(FILENAME))
+        print("Take {} seconds".format(time.time() - start_time), "\n")
 
 
-def create(path, name):
+def create(path, debug=0):
     """
 
-    :param path: str, path relative :param name: str, name directory
+    :param debug: debug flag
+    :param path: str, path relative
     :return: directories, create directories (ex:
     /Project/tmp/datalake/movies/movies.csv)for all csv files from unpack
     the directory and move csv files to them
     """
-    start_time = time.time()
+    # start_time = time.time()
     os.chdir(path)
-    print("Change directory to {}".format(name))
+    # print("Change directory to {}".format(name))
     for i in os.listdir(os.getcwd() + FOLDER):
         if i[-3:] == "csv":
             os.makedirs("tmp/datalake/{}".format(i[:-4]))
             shutil.move(os.getcwd() + FOLDER + "/" + i,
                         path + "/tmp/datalake/{}".format(i[:-4]))
-            print("{} directory create".format(i[:-4]))
-    print("Take {} seconds".format(time.time() - start_time), "\n")
+            if debug == 1:
+                print("{} directory create".format(i[:-4]))
+    # print("Take {} seconds".format(time.time() - start_time), "\n")
 
 
-def remove(filepath):
+def remove(filepath, debug=0):
     """
 
+    :param debug: debug flag
     :param filepath: str, file path relative
     :return: nothing, remove not used files/directories
     """
     start_time = time.time()
     os.remove(filepath)
-    print("Remove {} complete".format(FILENAME))
+    # print("Remove {} complete".format(FILENAME))
     shutil.rmtree(os.getcwd() + FOLDER)
-    print("Remove {} complete".format(FOLDER))
-    print("Take {} seconds".format(time.time() - start_time), "\n")
+    if debug == 1:
+        print("Remove {} complete".format(FOLDER))
+        print("Take {} seconds".format(time.time() - start_time), "\n")
 
 
-start_time = time.time()
-make_dir()
-download(FILEPATH)
-unpack(FILEPATH, PATH)
-create(PATH, NAME)
-remove(FILEPATH)
-print("Script take {} seconds".format(time.time() - start_time))
+def main(debug=0):
+    """
+
+    :param debug: debug flag
+    :return: main function
+    """
+    start_time = time.time()
+    make_dir()
+    download(FILEPATH)
+    unpack(FILEPATH, PATH)
+    create(PATH)
+    remove(FILEPATH)
+    if debug == 1:
+        print("Script take {} seconds".format(time.time() - start_time))
+
+
+if __name__ == '__main__':
+    main()
