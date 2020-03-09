aaccb55a268a49e156680956735caf20904d9479
Vadzim <vadi.vad.dvp@gmail.com>
2020-03-09 22:34:53 +0300

    v.0.0.2

--- map-reduce-sh/1.sh
+++ map-reduce-sh/1.sh
@@ -5,17 +5,11 @@ movies="movies"
 ratings="ratings"
 tags="tags"
 
-current_path=`pwd`
-mkdir data
-dir_path=${current_path}/data
-cd ${dir_path}
-wget http://files.grouplens.org/datasets/movielens/ml-latest.zip
-file_path=${dir_path}/`ls`
+dir_path=`pwd`/data
 tar_path=${dir_path}/tar
-mkdir -p ${tar_path}
-unzip ${file_path} -d ${tar_path}
 tmp_path=${dir_path}/tmp/datalake
-mkdir -p ${tmp_path}
+file_path=${dir_path}/ml-latest.zip
+cd ${dir_path}
 for var in $(ls ${tar_path}/`ls ${tar_path}`); do
 case ${var} in
     "${links}.csv")
