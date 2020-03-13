603fd62460f3630aed4a9a958803055a0c6455cc
Vadzim <vadi.vad.dvp@gmail.com>
2020-03-13 21:38:29 +0300

    v.1.0.1

--- map-reduce-sh/reducer.py
+++ map-reduce-sh/reducer.py
@@ -1,27 +1,6 @@
 import sys
-import os
-import json
 from numpy import nan
 import math
-import shutil
-
-# filename can be overwritten
-NAME = "output"
-# path of directory
-DIR = os.getcwd() + "/data/tmp/result"
-
-
-def check_dir():
-    """
-    Check directory, if directory exist delete them, and create new.
-    :return: Nothing
-    """
-    while True:
-        try:
-            os.mkdir(DIR)
-            break
-        except FileExistsError:
-            shutil.rmtree(DIR)
 
 
 def create_lists():
@@ -50,6 +29,7 @@ def create_tmp_dict(genres_list):
         value_tmp_dict = {"genre": f"{i}", "movies": []}
         tmp_dict[cnt] = value_tmp_dict
         cnt += 1
+
     return tmp_dict
 
 
@@ -66,18 +46,9 @@ def create_main_dict(tmp_dict, tuple_list):
                 if k[0] == j[1]:
                     i[1]["movies"].append(
                         {"title": f"{k[1][0]}",
-                         "year": int(k[1][1]) if not math.isnan(k[1][1]) else None})
-    return tmp_dict
-
-
-def create_json(main_dict):
-    """
-
-    :param main_dict: main dictionary
-    :return: create json file
-    """
-    with open(f"{DIR}/{NAME}.json", "w") as wr_file:
-        json.dump(main_dict, wr_file)
+                         "year": int(k[1][1]) if not math.isnan(
+                             k[1][1]) else None})
+        print(i[1])
 
 
 def main():
@@ -85,11 +56,10 @@ def main():
 
     :return: main func
     """
-    check_dir()
     genre_list, tuple_list = create_lists()
+
     tmp_dict = create_tmp_dict(genre_list)
-    main_dict = create_main_dict(tmp_dict, tuple_list)
-    create_json(main_dict)
+    create_main_dict(tmp_dict, tuple_list)
 
 
 if __name__ == '__main__':
