ed174a7842469dde951a439795ed42627360bb36
Vadzim <vadi.vad.dvp@gmail.com>
2020-03-15 22:55:14 +0300

    v.1.0.2

--- map-reduce-sh/mapper.py
+++ map-reduce-sh/mapper.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import sys
 import argparse
 
@@ -30,85 +29,133 @@ def parsing():
 def normalize():
     """
 
-    :return: normalize dataframe
+    :return: normalize dataset
     """
-    df = pd.read_csv(sys.stdin)
-    title_col = []
-    tmp_year_col = []
-    for i in df["title"]:
+    year = []
+    tmp_year = []
+    title = []
+    tmp = []
+    tmp_genres = []
+    genres = []
+    for line in sys.stdin:
+        line = line.strip()
+        line = line.split(',')
+        if len(line) > 3:
+            for i in range(2, len(line) - 1):
+                line[1] = line[1] + line[i]
+        line = "{}, {}, {}".format(line[0], line[1], line[-1])
+        line = line.split(",")
+        tmp.append(line[1])
+        tmp_genres.append(line[2])
+    for i in tmp:
         if i.rfind("(") == -1:
-            title_col.append(i)
+            title.append(i)
         else:
-            title_col.append(i[:i.rfind("(")])
+            title.append(i[:i.rfind("(")])
         if i.rfind("(") == -1 and i.rfind(")") == -1:
-            tmp_year_col.append(i)
+            tmp_year.append(i)
         else:
-            tmp_year_col.append(i[i.rfind("(") + 1:i.rfind(")")])
-    year_col = []
-    for i in tmp_year_col:
+            tmp_year.append(i[i.rfind("(") + 1:i.rfind(")")])
+    for i in tmp_year:
         try:
-            year_col.append(int(i))
+            year.append(int(i))
         except ValueError:
-            year_col.append(None)
-    df["title"] = title_col
-    df["year"] = year_col
-    format_genres = df["genres"].str.split("|")
-    df["genres"] = format_genres
-    df = df.drop(columns="movieId")
-    return df
+            year.append(None)
+    for i in tmp_genres:
+        genres.append(i.strip().split("|"))
+    main = list(zip(title, year, genres))
+    return main
 
 
-def mapping(df):
+def mapping(ds):
     """
 
-    :param df: dataframe
-    :return: tuple(genre, [title, year])
+    :param ds: dataset
+    :return: genre, [title, year]
     """
-    for i in df.index:
-        for j in df["genres"][i]:
-            map_row = (j, [df["title"][i], df["year"][i]])
-            print(map_row)
+    for i in ds:
+        for j in i[2]:
+            yield j, (i[0], i[1])
 
 
-def search(word, genres, year_from, year_to, df):
+def genres_helper(ds, genres):
+    """
+
+    :param ds: dataset
+    :param genres: genres flag
+    :return: ds with apply genres
+    """
+    genre_ds = []
+    for i in ds:
+        for j in i[2]:
+            if j == genres:
+                genre_ds.append(i)
+    return genre_ds
+
+
+def word_helper(ds, word):
+    """
+
+    :param ds: dataset
+    :param word: word flag
+    :return: dataset with apply word
+    """
+    word_ds = []
+    for i in ds:
+        if i[0].find(word) != -1:
+            word_ds.append(i)
+    return word_ds
+
+
+def time_helper(ds, year_from, year_to):
+    """
+
+    :param ds: dataset
+    :param year_from: year_from flag
+    :param year_to: year_to flag
+    :return: dataset with apply years
+    """
+    time_ds = []
+    for i in ds:
+        try:
+            if year_from <= i[1] <= year_to:
+                time_ds.append(i)
+        except TypeError:
+            continue
+    return time_ds
+
+
+def search(word, genres, year_from, year_to, ds):
     """
 
     :param word: word flag
     :param genres: genres flag
     :param year_from: year_from flag
     :param year_to: year_to flag
-    :param df: dataframe
-    :return: tuples(genre, title, year)
+    :param ds: dataset
+    :return: genre (title, year)
     """
+
     if genres is None and word is None:
-        mapping(df)
+        for key, value in mapping(ds):
+            print("{}\t{}".format(key, value))
     elif genres is None:
-        word_df = df.loc[
-            df["title"].str.contains(word, regex=False)
-        ]
-        word_year_df = word_df.loc[
-            (word_df["year"] > year_from) & (word_df["year"] < year_to)
-            ]
-        mapping(word_year_df)
+        word_ds = word_helper(ds, word)
+        word_time_ds = time_helper(word_ds, year_from, year_to)
+        for key, value in mapping(word_time_ds):
+            print("{}\t{}".format(key, value))
     elif word is None:
-        genres_df = df.loc[
-            df["genres"].str.contains(genres, regex=False)
-        ]
-        genres_year_df = genres_df.loc[
-            (genres_df["year"] > year_from) & (genres_df["year"] < year_to)
-            ]
-        mapping(genres_year_df)
+        genre_ds = genres_helper(ds, genres)
+        genre_time_ds = time_helper(genre_ds, year_from, year_to)
+        for key, value in mapping(genre_time_ds):
+            print("{}\t{}".format(key, value))
     else:
-        word_df = df.loc[
-            df["title"].str.contains(word, regex=False)
-        ]
-        genres_df = word_df.loc[
-            word_df["genres"].str.contains(genres, regex=False)
-        ]
-        word_genres_year_df = genres_df.loc[
-            (genres_df["year"] > year_from) & (genres_df["year"] < year_to)
-            ]
-        mapping(word_genres_year_df)
+        word_ds = word_helper(ds, word)
+        genre_ds = genres_helper(word_ds, genres)
+        word_genre_time_ds = time_helper(genre_ds, year_from, year_to)
+        for key, value in mapping(word_genre_time_ds):
+            print("{}\t{}".format(key, value))
+
 
 
 def main():
@@ -117,8 +164,8 @@ def main():
     :return: main func
     """
     word, genres, year_from, year_to = parsing()
-    df = normalize()
-    search(word, genres, year_from, year_to, df)
+    ds = normalize()
+    search(word, genres, year_from, year_to, ds)
 
 
 if __name__ == '__main__':
